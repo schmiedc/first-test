8c9df72b0c666a685a7d01c939aa690eb2bd31b2
christopher.schmied <10744409+schmiedc@users.noreply.github.com>
2022-03-22 15:28:05 +0100

    changed a comment string

--- src/first_test/_widget.py
+++ src/first_test/_widget.py
@@ -7,8 +7,8 @@ def flood_magic_factory(
         img_layer: napari.layers.Image, threshold: float = 0
 ) -> napari.types.LayerDataTuple:
 
-    label_image = img_layer.data <= threshold  # get image data from napari image layer and threshold
-    label_image = label_image.astype(int) * 13  # # label 13 is blue in napari
+    label_image = img_layer.data <= threshold  # # get numpy array from image layer, threshold image return binary
+    label_image = label_image.astype(int) * 13  # label 13 is blue in napari
 
     # create DataTuple
     label_seg = (label_image, {'name': 'seg'})
