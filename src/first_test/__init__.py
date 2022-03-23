905302eb9d1d4a998967181d287c85d84b76618c
christopher.schmied <10744409+schmiedc@users.noreply.github.com>
2022-03-23 17:13:37 +0100

    added test for thresholding function

--- src/first_test/__init__.py
+++ src/first_test/__init__.py
@@ -6,3 +6,4 @@ from ._reader import napari_get_reader
 from ._writer import write_single_image, write_multiple
 from ._sample_data import make_sample_data
 from ._widget import flood_magic_factory
+from ._widget import thresholding
--- src/first_test/_widget.py
+++ src/first_test/_widget.py
@@ -2,12 +2,21 @@ from magicgui import magic_factory
 import napari
 import imageio
 
+
 @magic_factory
 def flood_magic_factory(
         img_layer: napari.layers.Image, threshold: float = 0
 ) -> napari.types.LayerDataTuple:
+    if napari.layers.Image:
+        output_image = thresholding(img_layer.data, threshold)
 
-    label_image = img_layer.data <= threshold  # # get numpy array from image layer, threshold image return binary
+        return output_image
+    else:
+        napari.utils.notifications.Notification("error")
+
+
+def thresholding(image_data, threshold):
+    label_image = image_data <= threshold  # get numpy array from image layer, threshold image return binary
     label_image = label_image.astype(int) * 13  # label 13 is blue in napari
 
     # create DataTuple
